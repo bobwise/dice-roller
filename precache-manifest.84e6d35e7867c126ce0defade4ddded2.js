74fc0545f2dd7876351e1e9bde9026ad17ae72cb
Bob Wise <bobwise@quickenloans.com>
2020-05-07 17:29:34 -0400

    Updates

---
+++ precache-manifest.84e6d35e7867c126ce0defade4ddded2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06dc94527520c1dfacd48a9d6ff531a1",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "845d8feddf3f4c11d85c",
+    "url": "/dice-roller/static/css/main.d0e4f91b.chunk.css"
+  },
+  {
+    "revision": "303b26fa2cea808b3a60",
+    "url": "/dice-roller/static/js/2.b53e5185.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dice-roller/static/js/2.b53e5185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845d8feddf3f4c11d85c",
+    "url": "/dice-roller/static/js/main.3b174ff0.chunk.js"
+  },
+  {
+    "revision": "6b153dc6a273cab4dac4",
+    "url": "/dice-roller/static/js/runtime-main.3ad158a9.js"
+  }
+]);
